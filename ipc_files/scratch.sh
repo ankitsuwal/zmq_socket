6696456c8b9c64ca234fe3ced2912ae8a538f66c
AnkitSuwal <ankitsuwal@gmail.com>
2020-09-25 13:40:28 +0530

    added .gitignore file

---
+++ ipc_files/scratch.sh
@@ -0,0 +1,5 @@
+/home/adminp/anaconda3/envs/tfpose2/bin/python /home/adminp/bfl/projects/ipc_django/django_ipc/ipc_files/camera.py &
+/home/adminp/anaconda3/envs/tfpose2/bin/python /home/adminp/bfl/projects/ipc_django/django_ipc/ipc_files/ods.py 3333 &
+/home/adminp/anaconda3/envs/tfpose2/bin/python /home/adminp/bfl/projects/ipc_django/django_ipc/ipc_files/rds.py 4444 &
+/home/adminp/anaconda3/envs/tfpose2/bin/python /home/adminp/bfl/projects/ipc_django/django_ipc/ipc_files/local_planner.py 3333 4444 &
+#/home/adminp/anaconda3/envs/tfpose2/bin/python /home/adminp/bfl/projects/ipc/zmq_socket/zero_mq/django_test.py 3333 4444 &
